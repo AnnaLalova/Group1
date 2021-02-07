3a9092afaf6e0df1985ac65f13b614446322aba2
AnnaLalova <annalalova91@gmail.com>
2021-02-07 22:16:05 +0200

    ShellSort working+time+loops

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,51 +1,54 @@
 package com.company;
 
+import com.sun.tools.javac.Main;
+
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
+import java.time.Instant;
 
 
 
 
     class ShellSort {
         void shellSort(int array[], int n) {
+            int loop = 0;
             for (int interval = n / 2; interval > 0; interval /= 2) {
                 for (int i = interval; i < n; i += 1) {
                     int temp = array[i];
                     int j;
                     for (j = i; j >= interval && array[j - interval] > temp; j -= interval) {
                         array[j] = array[j - interval];
+                        loop++;
                     }
                     array[j] = temp;
                 }
             }
+            System.out.println("Loops iterated: " + loop);
         }
 
         public static void main(String[] args) throws InterruptedException {
             Scanner input = new Scanner(System.in);
             System.out.println("Enter the array size: ");
             int number = input.nextInt();
-            //System.out.println("Enter the array size: " + number);
             Random rd = new Random();
+            Instant start=Instant.now();
             int[] array = new int[number];
             for (int i = 0; i < array.length; i++) {
                 array[i] = rd.nextInt();
                 System.out.println(array[i]);
             }
-            ShellSort ss = new ShellSort();
+            ShellSort ss=new ShellSort();
             ss.shellSort(array, number);
             System.out.println("Sorted Array in Ascending Order: ");
             System.out.println(Arrays.toString(array));
+Instant finish=Instant.now();
+long timeElapsed = Duration.between(start,finish).toMillis();
+            System.out.println("That took " + timeElapsed + " MILLIseconds.");
 
-                long startTime = System.nanoTime();
-               Thread.sleep(5000);
-                long endTime = System.nanoTime();
-                //System.out.println("That took " + TimeUnit.MILLISECONDS.toSeconds((endTime-startTime)) + " seconds.");
-            System.out.println("Execution time in nanoseconds : " + (endTime-startTime));
-            }
-            }
-
+        }
+    }
 
 
 
